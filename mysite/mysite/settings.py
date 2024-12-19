38be4416af6cbdec27155086c8bb5a70073112f5
ritc4 <152070865+ritc4@users.noreply.github.com>
2024-12-19 22:53:11 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
         'USER': 'admin',
-        'PASSWORD': 'r12345ws',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
     }
